1ef3c8628899e7907f27126ccc220e4d67a17d02
Akwesi-bonah <arhinbonnah@gmail.com>
2024-01-04 23:02:44 +0000

    update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -5,23 +5,23 @@ import Car from './10-car';
  * @extends Car
  */
 export default class EVCar extends Car {
-    /**
+  /**
      * Creates a new instance of EVCar.
      * @param {string} brand - The brand of the car.
      * @param {string} motor - The type of motor used in the car.
      * @param {string} color - The color of the car.
      * @param {number} range - The range of the car in miles.
      */
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
-        this._range = range;
-    }
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
 
-    /**
+  /**
      * Clones the EVCar.
      * @returns {Car} - A new instance of Car.
      */
-    cloneCar() {
-        return new Car();
-    }
+  cloneCar() {
+    return new Car();
+  }
 }
