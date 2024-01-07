7ab9d5e4c6e383043797866a07a0daa3113f257e
Akwesi-bonah <arhinbonnah@gmail.com>
2024-01-07 19:46:42 +0000

    eror fixed

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,8 @@
+/**
+ * Iterates through an object and returns a string representation of its values.
+ * @param {Object} reportWithIterator - The object to iterate through.
+ * @returns {string} - The string representation of the object's values.
+ */
 /* eslint-disable radix */
 export default function iterateThroughObject(reportWithIterator) {
   let output = "";
--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -2,7 +2,8 @@
  * Creates an employee object with the given department name and employees.
  * @param {string} departmentName - The name of the department.
  * @param {Array} employees - An array of employees.
- * @returns {Object} - The employee object with the department name as the key and 
+ * @returns {Object} - The employee object 
+ * with the department name as the key and 
  * the employees as the value.
  */
 export default function createEmployeeObject(departmentName, employees) {
