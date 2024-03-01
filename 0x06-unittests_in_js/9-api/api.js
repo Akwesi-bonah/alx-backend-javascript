7c896d45238a403159d5b9a7863377c8f6f4a76b
Akwesi-Bona <arhinbonnah@gmail.com>
2024-03-01 11:09:05 +0000

    task completed

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,12 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 7865;
 
-app.get('/', (_, res) => {
-  res.send('Welcome to the payment system');
+app.get("/", (_, res) => {
+  res.send("Welcome to the payment system");
 });
 
+app.get("/cart/:id(\\d+)", (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get("/login", (req, res) => {
+  let name = "";
+
+  if (req.body) {
+    name = req.body.username;
+  }
+
+  res.send(`Welcome ${name}`);
+});
+
+app.get("avaliable_payment", (req, res) => {
+  res.json({
+    payment_methods: {
+      credits_cards: true,
+      paypal: false,
+    },
+  });
+});
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
